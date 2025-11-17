f5bbefb1a90353607c2a7daaaacfc1ddfe5498e2
AralGngr <aralgngr4@gmail.com>
2025-11-17 23:18:45 +0400

    Create variables.js

---
+++ Module-2/variables.js
@@ -0,0 +1,22 @@
+"John";
+
+var person;
+
+var person = "John";
+
+person;
+
+console.log("Hello", person);
+
+var greeting = "Hello";
+
+console.log(greeting, person);
+
+greeting = "Hi";
+
+person = "James";
+
+console.log(greeting);
+console.log(person);
+
+console.log(greeting, person);
