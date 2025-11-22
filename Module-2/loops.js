b9350daab7e079ea601ec7d3d5a5a2acd5576cd6
AralGngr <aralgngr4@gmail.com>
2025-11-22 14:44:11 +0400

    Create loops.js

---
+++ Module-2/loops.js
@@ -0,0 +1,29 @@
+for (var i =1; i<=5; i++) {
+    console.log(i);
+}
+console.log('Counting completed!');
+//
+for(var i=5; i>=1; i--) {
+    console.log(i )
+}
+console.log('Countdown finished')
+//
+var number=1
+while(number<=5) {
+    console.log( number);
+    number++
+}
+console.log('Counting completed!')
+//
+var number=5
+while(number>=1){
+    console.log(number)
+    number--
+}
+console.log('Countdown finished!')
+//
+var number=2018
+while(number<=2022) {
+    console.log( number)
+    number++
+}
