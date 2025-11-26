d0af9443a947ae847e640d22dccb5cdd207e651d
AralGngr <aralgngr4@gmail.com>
2025-11-26 18:31:33 +0400

    Create functions-and-arrays.js

---
+++ Module-2/functions-and-arrays.js
@@ -0,0 +1,16 @@
+
+let clothes = ["Shirt", "Pants", "Jacket", "Shoes"];
+
+console.log("Number of items in the closet:", clothes.length);
+
+function listClothing() {
+console.log("--- List of Clothes ---");
+
+
+for (let i = 0; i < clothes.length; i++) {
+console.log(clothes[i]);
+}
+}
+
+
+listClothing();
