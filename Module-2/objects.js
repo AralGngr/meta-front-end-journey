6aa2b4ad52b01e803f2cced317b52ad639882d06
AralGngr <aralgngr4@gmail.com>
2025-11-29 23:48:39 +0400

    Create objects.js

---
+++ Module-2/objects.js
@@ -0,0 +1,19 @@
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]])
+} 
+//
+//creating an object with properties and their values
+var assistantManager = {
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make some money"
+}
